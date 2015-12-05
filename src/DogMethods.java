54bf5122173048481f1f9ea0791521ce6598848b
OliviaImner <olivia.imner@hotmail.com>
2015-12-05 16:24:59 +0100

    added DogMethods

---
+++ src/DogMethods.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class DogMethods {
+	Scanner Keyboard = new Scanner(System.in);
+
+	//register methods
+	ArrayList<DogClass> RegisterCommmand(ArrayList<DogClass> currentRegister) {
+			
+			System.out.print("Please enter name: ");
+		    String Name = Keyboard.nextLine();
+		    
+		    System.out.print("Please enter breed: ");
+		    String Breed = Keyboard.nextLine();
+		    
+		    System.out.print("Please enter age: " );
+		    int Age = Keyboard.nextInt();
+		    Keyboard.nextLine();
+	
+		    System.out.print("Please enter weight: " );
+		    int Weight = Keyboard.nextInt();
+		    Keyboard.nextLine();
+			//Keyboard.close(); //closing standard in here results in an error since its still needed in "main"
+
+			DogClass newDog = new DogClass(Name, Breed, Age, Weight);
+			currentRegister.add(newDog);
+
+		    return currentRegister;
+		}
+	
+	//delete methods
+	ArrayList<DogClass> DeleteCommmand(ArrayList<DogClass> currentRegister) {
+		System.out.print("Please enter the name of the dog you want to delete from the register: ");
+	    String toDelete = Keyboard.nextLine();
+	    
+		int size = currentRegister.size();
+		for( int i=0; i < size; i++ ) {
+			DogClass currentDog = currentRegister.get(i);
+			String dogName = currentDog.getName();
+			if(dogName.equals(toDelete)){
+				currentRegister.remove(i);
+			}
+		}
+		return currentRegister;
+	}
+	
+	//list method
+	void ListCommand(ArrayList<DogClass> currentRegister) {
+		
+	}
+
+}
